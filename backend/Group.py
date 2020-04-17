a53a5f935f8ccb10ed7fe8e2fbddf7fd91342a89
Andy Bao <andybao@berkeley.edu>
2020-04-17 22:29:05 +1200

    finished group init, add and remove

--- backend/Group.py
+++ backend/Group.py
@@ -1,24 +1,136 @@
+import base64
+import requests
+import json
+import boto3
+import User
+import random
+
 class Group:
 
-	def __init__(self, groupname, hostname):
-		self.groupname = groupname
-		self.groupID = None # we need to find a way to hash new Ids or something
+	def __init__(self, group_name=None, host=None, group_id=None):
+		# creation of the initial group
+		dynamodb = boto3.resource('dynamodb')
+		self.dynamodb = dynamodb
+		group_table = dynamodb.Table('group')
+		if group_name is not None and host is not None:
+			# create a group with the host as the first member
+			self.group_name = group_name
+			self.group_id = str(random.randrange(10000,1000000000))
+			self.host_name = host.get_username()
 
-		self.users = [hostname]
-		
-		# store everything you need in the database(s)
+			# add host as the first member
+			self.users = {host.get_username()}
 
+			# create the group playlist and get a group_playlist_id
+			self.group_playlist_id = self.create_group_playlist(host, group_name)
 
+			# generate a random password for future members to join the group
+			self.access_code = random.randrange(100000, 999999)
+			
+			# store all the information in the database
+			group_table.put_item(Item={
+				'group_id': self.group_id,
+				'group_name': self.group_name,
+				'host_name' : self.host_name,
+				'users' : self.users,
+				'access_code' : self.access_code,
+				'group_playlist_id' : self.group_playlist_id
+			})
+			
+		# getting one single group from the database and instantiating it
+		else:
+			# get all the data from the database
+			try:
+				response = group_table.get_item(Key={
+					'group_id' : group_id
+					})
+			except ClientError as e:
+				return
+			else:
+				group_data = response['Item']
+				# instantiate the object given the data
+				self.group_id = group_id
+				self.group_name = group_data['group_name']
+				self.host_name = group_data['host_name']
+				self.users = group_data['users']
+				self.access_code = group_data['access_code']
+				self.group_playlist_id = group_data['group_playlist_id']
 
+	def add_member(self, user, input_access_code):
+		# add the member to the group object
+		# only if the input_access_code matches the access_code
+		# return None if input_access_code is incorrect, return the group playlist_id if correct
+		if input_access_code != self.access_code:
+			return
 
-	def create_group_playlist(self, groupname):
-		# create a group playlist
+		self.users.add(user.get_username())
+		self.add_group_member(user.get_username())
+		return self.group_playlist_id
 
-	def add_member(self, userId):
+	def remove_member(self, host_name, member_username):
+		# only the host has the ability to remove a member
+		# write the value to the database
+		if host_name == self.host_name:
+			member = User(member_username)
+			member.leave_group(self)
 
-	def remove_member(self, userId):
+	def leave_member(self, member_username):
+		self.users.remove(member_username)
+		self.remove_member_from_database(member_username)
+	
+	def remove_member_from_database(self, member_username):
+		group_table = self.dynamodb.Table('group')
+		group_table.update_item(
+			Key= {'group_id': self.group_id},
+			UpdateExpression= "DELETE #u :m",
+			ExpressionAttributeNames= {"#u": "users"},
+			ExpressionAttributeValues= {":m" :{member_username}}
+			)
 
-	def refresh_group_playlist(self):
+	def create_group_playlist(self, host, group_name):
+		# create an empty group playlist, returns the group_playlist_id
+		# get access token and create headers
+		print("creating playlist")
+		access_token = host.get_access_token()
+		headers =  {'Authorization': 'Bearer ' + access_token, 'Content-Type': 'application/json', 'Accept': 'application/json'}
+
+		# create the playlist under the host's account
+		data = {'name' : group_name}
+		creation_result = requests.post('https://api.spotify.com/v1/users/' + host.get_username() + '/playlists', data=json.dumps(data), headers=headers)
+		print(creation_result)
+		playlist_id = creation_result.json()['id']
+		return playlist_id
+
+	def add_group_member(self, member_username):
+		group_table = self.dynamodb.Table('group')
+		group_table.update_item(
+			Key= {"group_id" : self.group_id},
+			UpdateExpression="ADD #u :m",
+			ExpressionAttributeNames={"#u": "users"},
+			ExpressionAttributeValues={
+				':m': {member_username},
+			}
+			)
+
+	#def refresh_group_playlist(self):
 		# the weekly update
+	
+	def get_group_name(self):
+		return self.group_name
+	
+	def get_group_id(self):
+		return self.group_id
+		
+	def get_users(self):
+		return self.users
+	
+	def get_access_code(self):
+		return self.access_code
+	
+	def get_host_name(self):
+		return self.host_name
+	
+	def get_group_playlist_id(self):
+		return self.group_playlist_id
 
 	
--- backend/User.py
+++ backend/User.py
@@ -2,75 +2,147 @@ import base64
 import requests
 import json
 import boto3
+from Group import Group
+from datetime import datetime
 
 class User:
 	
 	# to create a new instance just call this in the main class that you create: user_object = User(username, usertoken)
-	def __init__(self, username, refreshToken):
-		# change the parameters if you need
-		self.username = username
-		self.groupIds = []
-		self.refreshToken = refreshToken
-
-		# create a new weekly favourites playlist
-		self.weeklyFavId = self.initialize_weekly_fav()
-
-		# store everything that we need in their respective databases
+	def __init__(self, username, refreshToken = None):
 		dynamodb = boto3.resource('dynamodb')
-		table = dynamodb.Table('user')
-		table.put_item(Item={
-			'username': self.username,
-			'groupIds': self.groupIds,
-			'refreshToken': self.refreshToken,
-			'weeklyFavId' : self.weeklyFavId
-								
+		user_table = dynamodb.Table('user')
+		response = user_table.get_item(Key={
+			'username' : username
 		})
+		self.dynamodb = dynamodb
 		
+		if 'Item' not in response:
+			self.username = username
+			self.groupIds = {"None"}
+			self.refreshToken = refreshToken
+		
+			#get an access code for the initialization process and create the headers field 
+			access_token = self.get_access_token()
+			headers =  {'Authorization': 'Bearer ' + access_token, 'Content-Type': 'application/json', 'Accept': 'application/json'}
+		
+			# create a new weekly favourites playlist
+			self.weeklyFavId = self.initialize_weekly_fav(headers)
+				
+			#get the top 50 tracks from the past month to store for history and for the next update
+			self.topFifty = self.top_tracks(headers, 50)
+	
+			# store everything that we need in their respective databases
+			self.store_user()
+			self.store_history(self.topFifty)
+		else:
+			user_data = response['Item']
+			# instantiate the object given the data
+			self.username = username
+			self.groupIds = user_data['groupIds']
+			self.refreshToken = user_data['refreshToken']
+			self.weeklyFavId = user_data['weeklyFavId']
+			self.topFifty = user_data['topFifty']
+			
+			
+
 	
 	#using the refresh token that is stored, get an access token
 	def get_access_token(self):
 		post_data = {'grant_type' : 'refresh_token', 'refresh_token' : self.refreshToken}
-		print(post_data)
 		#you need to supply the clientid:clientsecret encoded in base64 which are given to us when we register our app with spotify
 		post_headers = {'Authorization' : 'Basic MTAzZGY4ZjNhNDQ4NGI1MTkzYjcwMjljMTU1OGY5NGI6ZDUzN2Y4NDA1MDk2NDU2MGI5MWRjZWY2YTJiZDE0YmI='}
 		result = requests.post('https://accounts.spotify.com/api/token', data=post_data, headers=post_headers)
 		access_token = result.json()['access_token']
 		return access_token
+	
+	def get_headers(self):
+		access_token = self.get_access_token()
+		headers =  {'Authorization': 'Bearer ' + access_token, 'Content-Type': 'application/json', 'Accept': 'application/json'}
+		return headers
 		
-	def top_tracks(self, headers):
-		result = requests.get("https://api.spotify.com/v1/me/top/tracks?time_range=short_term", headers=headers)
+	def top_tracks(self, headers, limit):
+		result = requests.get("https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=" + str(limit), headers=headers)
 		list_r = []
 		for item in result.json()['items']:
 			list_r += [item['id']]
 		return list_r
 
 	def create_playlist(self, headers):
-		data = {'name' : 'weekly favorites'}
-		result = requests.post('https://api.spotify.com/v1/users/' + 'misho2211' + '/playlists', data=json.dumps(data), headers=headers )
+		data = {'name' : 'my current top 20'}
+		result = requests.post('https://api.spotify.com/v1/users/' + self.username + '/playlists', data=json.dumps(data), headers=headers )
 		return result.json()['id']
 		
-
-	def initialize_weekly_fav(self):
-		access_token = self.get_access_token()	
-		print(access_token)
-		headers =  {'Authorization': 'Bearer ' + access_token, 'Content-Type': 'application/json', 'Accept': 'application/json'}
-		
-		top_tracks = self.top_tracks(headers)
-		print("top_tracks")
-		print(top_tracks)
+	def initialize_weekly_fav(self, headers):
+		top_tracks = self.top_tracks(headers, 20)
 		playlist_id = self.create_playlist(headers)
-		print("playlist_id")
-		print(playlist_id)
-		
 		track_ids = ["spotify:track:" + x for x in top_tracks]
 		data = {'uris' : track_ids}
 		result = requests.post('https://api.spotify.com/v1/playlists/' + playlist_id + '/tracks', data=json.dumps(data), headers=headers)
+		
 		return playlist_id
 		
+	def update_weekly_fav(self, headers):
+		top_tracks = self.top_tracks(headers, 20)
+		playlist_id = self.weeklyFavId
+		track_ids = ["spotify:track:" + x for x in top_tracks]
+		data = {'uris' : track_ids}
+		result = requests.put('https://api.spotify.com/v1/playlists/' + playlist_id + '/tracks', data=json.dumps(data), headers=headers)
 
-
+	
+	def store_history(self, topFifty):
+		history_table = self.dynamodb.Table('history')
+		history_table.put_item(Item={
+			'username,date' : self.username + str(datetime.now()),
+			'songs' : topFifty
+		})
 		
+	def store_user(self):
+		user_table = self.dynamodb.Table('user')
+		user_table.put_item(Item={
+			'username': self.username,
+			'groupIds': self.groupIds,
+			'refreshToken': self.refreshToken,
+			'weeklyFavId' : self.weeklyFavId,
+			'topFifty' : self.topFifty
+		})
+	
+	#####
+	
+	def create_group(self, groupname):
+		# create the group with the host as the first member
+		group = Group(group_name = groupname, host = self, group_id = None)
+		self.groupIds.add(group.group_id)
+		self.store_user()
+
+	def join_group(self, groupid, access_code):
+		group = Group(group_name = None, host = None, group_id = groupid)
+		# group.add_member returns a boolean of whether the access_code is correct or not
+		group_playlist_id = group.add_member(self, access_code)
+		if group_playlist_id is not None:
+			self.groupIds.add(group.get_group_id())
+			# now make this user follow the host's group playlist
+			headers = self.get_headers()
+			result = requests.put('https://api.spotify.com/v1/playlists/{playlist_id}/followers' + group_playlist_id, headers=headers)
+			self.store_user()
+			
 
 
+	def leave_group(self, groupid):
+		group = Group(group_id = groupid)
+		group.leave_member(self.username)
+		self.groupIds.remove(groupid)
+		self.remove_groupId_from_database(groupid)
+	
+	def remove_groupId_from_database(self, groupid):
+		user_table = self.dynamodb.Table('user')
+		user_table.update_item(
+			Key = {"username": self.username},
+			UpdateExpression= "DELETE groupIds :g",
+			ExpressionAttributeValues= {":g" :{groupid}}
+			)
 
-#
+	def remove_member(self, group, member_username):
+		group.remove_member(self.username, member_username)
+	
+	def get_username(self):
+		return self.username
